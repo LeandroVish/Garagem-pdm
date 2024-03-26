47187aa972576c883cbd0ad2bacf8016dc4f4829
LeandroVish <leandrogustavosantos.info@gmail.com>
2024-03-26 08:36:34 -0300

    Criação da API para Acessórios

--- app/urls.py
+++ app/urls.py
@@ -9,11 +9,12 @@ from drf_spectacular.views import (
 from rest_framework.routers import DefaultRouter
 from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 
-from core.views import UserViewSet
+from core.views import UserViewSet, AcessoriosViewSet
 
 router = DefaultRouter()
 
 router.register(r"users", UserViewSet, basename="users")
+router.register(r"acessorios", AcessoriosViewSet, basename="acessorios")
 
 urlpatterns = [
     path("admin/", admin.site.urls),
